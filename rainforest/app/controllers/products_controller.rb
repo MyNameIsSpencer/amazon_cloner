e4965c45ba5edd9b20fe7d9b9add73e8daadad2e
spencer kyonka <spencer.kyonka@hotmail.ca>
2017-11-28 12:23:33 -0500

    fill in products controller

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -1,31 +1,55 @@
 class ProductsController < ApplicationController
 
   def index
-
+    @product = Product.all
   end
 
   def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description [:product][:description]
+    @product.price_in_cents [:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
 
   end
 
   def new
-
+    @product = Product.new
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def show
-
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.name = params[:product][:name]
+    @product.description [:product][:description]
+    @product.price_in_cents [:product][:price_in_cents]
 
+    if @product.save
+      redirect_to products_path(@product)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:id])
+    if @product.destroy
+      redirect_to product_path
+    else
+      render :show
+    end
   end
 
 end
