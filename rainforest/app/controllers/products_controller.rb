534b4cb60679d5718fb332b5e0438d8f2467d4ba
bluepeartree <sherrie.sebastian@gmail.com>
2017-11-28 13:54:19 -0500

    new product form added

--- rainforest/app/controllers/products_controller.rb
+++ rainforest/app/controllers/products_controller.rb
@@ -7,8 +7,8 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new
     @product.name = params[:product][:name]
-    @product.description [:product][:description]
-    @product.price_in_cents [:product][:price_in_cents]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
 
     if @product.save
       redirect_to products_path
